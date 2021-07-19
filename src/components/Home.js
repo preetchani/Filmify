65b8701523f0dd0459f71aa722b1e8435789ac3d
preetchani <preetchani23498@gmail.com>
2021-07-19 21:19:22 +0530

    Grid view for Movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import { useHomeFetch } from '../hooks/useHomeFetch';
 import { POSTER_SIZE,BACKDROP_SIZE,IMAGE_BASE_URL } from '../config';
 import NotFound from '../images/no_image.jpg';
 import MainSection from './MainSection/mainSection';
+import Grid from './Grid/grid';
+import Thumb from './Thumbnails/thumb';
 const Home=()=>{
    const {state,loading,error} = useHomeFetch();
     //console.log(`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`);
@@ -11,12 +13,22 @@ const Home=()=>{
         
         {state.results[0] ?
             <MainSection 
-            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+            image={IMAGE_BASE_URL+BACKDROP_SIZE+state.results[0].backdrop_path}
             title={state.results[0].original_title}
             text={state.results[0].overview}
             />
             :null
         }
+        <Grid header="Popular Movies">
+        {state.results.map(movies=>(
+            <Thumb
+             key={movies.id}
+             clickable 
+             image={movies.poster_path ? IMAGE_BASE_URL + POSTER_SIZE + movies.poster_path : NotFound} 
+             movieId={movies.id}/>
+        ))}
+        </Grid>
+        
         </>
     )
 }
